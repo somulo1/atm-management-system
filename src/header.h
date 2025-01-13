ea6b5c74ee9d37c27763e0c64a0b64bee11da6ad
somulo <mcomulosammy37@gmail.com>
2025-01-13 15:31:53 +0300

    update header structs

--- src/header.h
+++ src/header.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
 
 struct Date
 {
@@ -12,17 +14,17 @@ struct Record
 {
     int id;
     int userId;
-    char name[100];
-    char country[100];
-    int phone;
+    char name[50];
+    char country[70];
+    char phone[16];
     char accountType[10];
-    int accountNbr;
+   // char accountNbr;
+   char accountNbr[20];
     double amount;
     struct Date deposit;
     struct Date withdraw;
 };
-int isValidName(const char *name);
-int isValidDate(int day, int month, int year);
+
 struct User
 {
     int id;
@@ -32,10 +34,34 @@ struct User
 
 // authentication functions
 void loginMenu(char a[50], char pass[50]);
-void registerMenu(char a[50], char pass[50]);
-const char *getPassword(struct User u);
+void signUpMenu(char a[50], char pass[50]);
+const char *getPassword(struct User *u);
 
-// system function
-void createNewAcc(struct User u);
+// system functions
 void mainMenu(struct User u);
+void createNewAcc(struct User u);
+void transferAccount(struct User u, char *accountNum);
+void checkDetailOfAccount(struct User u, char *accountID);
+void updateAccountInfo(struct User u, char *accountNum, int choice);
+void makeTransaction(struct User u, char *accountNum, int choice);
+void removeAccount(struct  User u, char *accountNum);
 void checkAllAccounts(struct User u);
+void checkAccountBalance(struct User u, char *accountID);
+void transactionAcc(struct User u, char *accountNum);
+
+
+
+//secondary functions
+bool isNamePresent(char *name);
+bool isPresent(char array[100][100], char* str);
+bool isAccountPresent(char *num, struct User u);
+bool isAccountPresentAny(char *num, struct User u);
+int getUserId(char *name);
+int getRecordId(char *name);
+bool checkUsers();
+bool checkRecords();
+void alphamirror(char *str);
+bool is_valid_number();
+bool is_valid_string(char *strInput);
+void clearInputBuffer();
+void sanitize(char *str);
